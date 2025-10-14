29e1428d638179353f19cf71c8102c38fb31eedc
mpb14 <lyftc@126.com>
2025-10-14 12:07:12 +0800

    feat: remove ios

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,2 @@
-import { Platform } from 'react-native';
-import type { KioskManagerType } from './KioskManager.type';
-
-let KioskManager: KioskManagerType;
-if (Platform.OS === 'android') {
-  KioskManager = require('./index.android').default;
-} else {
-  KioskManager = require('./index.ios').default;
-}
-
+import KioskManager from './index.android';
 export default KioskManager;
